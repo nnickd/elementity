349a2d88a58e0344d44fa8949e03c2caf22d76ba
nicolas.dellitalia <nicolas.dellitalia@stsaviationgroup.com>
2020-06-14 23:13:53 -0400

    copy app.js from public

--- app.js
+++ app.js
@@ -1,128 +1,63 @@
-var Engine = Matter.Engine,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite,
-    Vector = Matter.Vector;
-
 var socket = io();
 
-var engine;
-var world;
-var entity;
-var food;
-var elements = [];
-var boundaries = [];
+var elements = {
+    pos: [],
+    ang: [],
+    rad: []
+}
 
+var foods = {
+    pos: [],
+    ang: [],
+    rad: []
+}
 
-socket.on('mouse', function(mx, my) {
-    console.log(mx, my);
+socket.on('render elements', function(pos, ang, rad) {
+    elements.pos = pos;
+    elements.ang = ang;
+    elements.rad = rad;
+});
+
+socket.on('render foods', function(pos, ang, rad) {
+    foods.pos = pos;
+    foods.ang = ang;
+    foods.rad = rad;
 });
 
 function setup() {
     createCanvas(800, 800);
-    engine = Engine.create();
-    world = engine.world;
-    food = Composite.create();
-    World.add(world, food);
-    walls(10);
-    world.gravity.y = 0;
-    randomFood(300);
-    entity = new Circle(random(20, width - 20), random(20, height - 20), random(1, 3));
-    elements.push(entity);
-    // Engine.run(engine);
-}
-
-function randomFood(amount) {
-    for (var i = 0; i < amount; i++) {
-        new Food(random(20, width - 20), random(20, height - 20), 3);
-    }
-}
-
-function walls(thick) {
-    boundaries.push(new Boundary(width / 2, height - thick / 2, width, thick, 0))
-    boundaries.push(new Boundary(width / 2, thick / 2, width, thick, 0))
-    boundaries.push(new Boundary(thick / 2, height / 2, thick, height, 0))
-    boundaries.push(new Boundary(width - thick / 2, height / 2, thick, height, 0))
-}
-
-function ground(thick) {
-    boundaries.push(new Boundary(width / 2, height - thick / 2, width, thick, 0))
-}
-
-function mousePressed() {
-    elements.push(new Circle(mouseX, mouseY, random(1, 3)));
-}
-
-function mouseDragged() {
-    elements.push(new Circle(mouseX, mouseY, random(1, 3)));
 }
 
 function draw() {
     background(51);
-    Engine.update(engine);
-    tick();
-    show();
+    socket.emit('mouse', mouseX, mouseY)
+    renderFoods();
+    renderElements();
 }
 
-function tick() {
-    var dead = [];
-
-    for (var i = elements.length - 1; i >= 0; i--) {
-        elements[i].seek({
-            x: mouseX,
-            y: mouseY
-        }); 
-        console.log(elements[i].body.bounds)
-        var near = Matter.Query.region(food.bodies, elements[i].body.bounds);
-        for (var j = 0; j < near.length; j++) {
-            elements[i].absorb(near[j]);
-            Composite.remove(food, near[j]);
-            randomFood(1);
-        }
-    }
-
-    for (var i = elements.length - 1; i >= 0; i--) {
-        var near = Matter.Query.region(world.bodies, elements[i].body.bounds);
-        for (var j = 0; j < near.length; j++) {
-            if (elements[i].body.mass > near[j].mass) {
-                elements[i].absorb(near[j]);
-                World.remove(world, near[j])
-                dead.push(near[j]);
-            }
-        }
-    }
-
-    for (var i = elements.length - 1; i >= 0; i--) {
-        for (var j = 0; j < dead.length; j++) {
-            if (elements[i].body === dead[j]) {
-                elements.splice(i, 1);
-                dead.splice(j, 1);
-                break;
-            }
-        }
-    }
-}
-
-function show() {
-    for (var i = elements.length - 1; i >= 0; i--)
-        elements[i].show();
-    for (var i = 0; i < boundaries.length; i++)
-        boundaries[i].show();
-    showFood();
-}
-
-function showFood() {
-    for (var i = 0; i < food.bodies.length; i++) {
-        var body = food.bodies[i];
-        var pos = body.position;
-        var angle = body.angle;
+function renderFoods() {
+    for (var i = 0; i < foods.pos.length; i++) {
         push();
-        translate(pos.x, pos.y);
-        rotate(angle);
+        translate(foods.pos[i].x, foods.pos[i].y);
+        rotate(foods.ang[i]);
         rectMode(CENTER);
-        fill(color(0, 255, 0));
+        fill(color(0, 255, 100));
         noStroke();
-        ellipse(0, 0, body.circleRadius * 2);
+        ellipse(0, 0, foods.rad[i] * 2);
+        pop();
+    }
+}
+
+function renderElements() {
+    for (var i = 0; i < elements.pos.length; i++) {
+        push();
+        colorMode(HSB)
+        translate(elements.pos[i].x, elements.pos[i].y);
+        rotate(elements.ang[i]);
+        rectMode(CENTER);
+        fill(color(270, 100, 100));
+        noStroke();
+        ellipse(0, 0, elements.rad[i] * 2);
         pop();
     }
 }
